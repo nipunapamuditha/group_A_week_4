8f4c7ac949db8a20be7bf70368bf08ad7b90770e
nipunapamuditha <nipunapamuditha234@gmail.com>
2024-09-27 15:38:22 -0400

    updated nipuna

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group A's Week 4 Project!")
+	fmt.Println("Welcome to Group A's Week 4 Project!")
+	result := concatStrings("Hello, ", "World!")
+	print(result)
 }
