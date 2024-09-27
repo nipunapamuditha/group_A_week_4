b08b6ef052a8d016d99472f428a21bbfcbde9059
Ashishpoudel65 <ashishpoudel@loyalistcollege.com>
2024-09-27 16:22:34 -0400

    Update ashish_poudel.go

--- ashish_poudel.go
+++ ashish_poudel.go
@@ -4,20 +4,16 @@ import "fmt"
 
 func agecount() {
 
-	// declare a struct
 	type Person struct {
 		name string
 		age  int
 	}
-
-	// assign value to struct while creating an instance
+	
 	person1 := Person{"SHAN", 25}
 	fmt.Println(person1)
 
-	// define an instance
 	var person2 Person
 
-	// assign value to struct variables
 	person2 = Person{
 		name: "NIPUNA",
 		age:  29,
