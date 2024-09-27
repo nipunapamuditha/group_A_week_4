3fba678279267153fc291ba6e30326bf99d46e27
nipunapamuditha <nipunapamuditha234@gmail.com>
2024-09-27 15:58:55 -0400

    s

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Group A's Week 4 Project!")
-	result := concatStrings("Hello, ", "World!")
+	result := concatStrings("Nipuna, ", "Karunarathna")
 	print(result + "\n")
 	print("Seruban Peter Shan \n")
 	SerubanPeterShan(8)
